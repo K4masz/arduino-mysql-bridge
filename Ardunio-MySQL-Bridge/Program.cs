5ccc7fc2374fec5837ef3355796ee3931e8e2ba1
K4masz <k4masz@gmail.com>
2018-05-16 17:20:30 +0200

    Add project files.

---
+++ Ardunio-MySQL-Bridge/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ardunio_MySQL_Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+    
+           
+        }
+    }
+}
