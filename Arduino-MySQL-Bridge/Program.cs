6ca9ab6bbd6a8d46f4c8e8bad58039a66c67cf16
K4masz <k4masz@gmail.com>
2018-05-26 17:45:49 +0200

    Added:  - testing select query  - testing insert query

--- Arduino-MySQL-Bridge/Program.cs
+++ Arduino-MySQL-Bridge/Program.cs
@@ -11,6 +11,31 @@ namespace Ardunio_MySQL_Bridge
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var dbCon = DbConnection.Instance();
+            dbCon.DatabaseName = "weather_station";
+            if (dbCon.IsConnect())
+            {
+                string query = "select * from measurements;";
+                var cmd = new MySqlCommand(query, dbCon.Connection);
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader.GetInt16("measurement_id"));
+                    Console.WriteLine(reader.GetDouble("s1_humidity"));
+                    Console.WriteLine(reader.GetDouble("s2_humidity"));
+                }
+
+                reader.Close();
+
+                string query2 =
+                    string.Format(
+                        "Insert Into measurements (s1_humidity, s2_humidity, s1_temperature, s2_temperature, light_sensor_1, light_sensor_2, time) values ({0},{1},{2},{3},{4},{5},{6});",
+                        20, 20, 20, 30, 40, 50, "now()");
+                var cmd2 = new MySqlCommand(query2, dbCon.Connection);
+                cmd2.ExecuteNonQuery();
+            }
+
             Console.ReadKey();
         }
     }
@@ -21,9 +46,9 @@ namespace Ardunio_MySQL_Bridge
         {
         }
 
-        private string DatabaseName { get; set; } = string.Empty;
+        public string DatabaseName { get; set; } = string.Empty;
         private string Password { get; set; }
-        private MySqlConnection Connection { get; set; }
+        public MySqlConnection Connection { get; set; }
         private static DbConnection _instance = null;
 
         public static DbConnection Instance()
@@ -38,7 +63,9 @@ namespace Ardunio_MySQL_Bridge
                 if (String.IsNullOrEmpty(DatabaseName))
                     return false;
                 string connstring =
-                    string.Format("Server=localhost;Port=3306;Uid=Arduino;password=arduino;", DatabaseName);
+                    string.Format(
+                        "Server=192.168.0.101;database={0};Port=3306;Uid=ArduinoRemote;password=arduinoremote;SslMode=none;",
+                        DatabaseName);
                 Connection = new MySqlConnection(connstring);
                 Connection.Open();
             }
@@ -48,7 +75,6 @@ namespace Ardunio_MySQL_Bridge
 
         public void Close()
         {
-
             if (this.IsConnect())
             {
                 Connection.Close();
